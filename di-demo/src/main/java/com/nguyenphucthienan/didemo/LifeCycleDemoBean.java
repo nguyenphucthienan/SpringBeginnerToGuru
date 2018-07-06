df01415a26f4b9462c0a6fc897d014e3f1d05888
Nguyen Phuc Thien An <npta97@gmail.com>
2018-07-06 13:12:35 +0700

    Add LifeCycleDemoBean

--- di-demo/src/main/java/com/nguyenphucthienan/didemo/LifeCycleDemoBean.java
+++ di-demo/src/main/java/com/nguyenphucthienan/didemo/LifeCycleDemoBean.java
@@ -1,4 +1,61 @@
-package com.nguyenphucthienan.didemo.service;
+package com.nguyenphucthienan.didemo;
 
-public class LifeCycleDemoBean {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Component
+public class LifeCycleDemoBean implements InitializingBean, DisposableBean, BeanNameAware,
+        BeanFactoryAware, ApplicationContextAware {
+    public LifeCycleDemoBean() {
+        System.out.println("## 1. LifeCycleBean constructor");
+    }
+
+    public void beforeInit() {
+        System.out.println("## 4. beforeInit - called by BeanPostProcessor");
+    }
+
+    public void afterInit() {
+        System.out.println("## 7. afterInit - called by BeanPostProcessor");
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+        System.out.println("## 6. afterPropertiesSet");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("## 9. destroy. The lifecycle Bean has been terminated");
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("## 2. setBeanName. Bean name is: " + name);
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("## 3. setBeanFactory");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        System.out.println("## 5. @PostConstruct");
+    }
+
+    @PreDestroy
+    public void PreDestroy() {
+        System.out.println("## 8. @PreDestroy");
+    }
 }
