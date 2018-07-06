78b6e5a1ff1da9c088617bab65af7735f1546990
Nguyen Phuc Thien An <npta97@gmail.com>
2018-07-06 11:30:13 +0700

    Profile annotation

--- di-demo/src/main/java/com/nguyenphucthienan/didemo/service/PrimarySpanishGreetingService.java
+++ di-demo/src/main/java/com/nguyenphucthienan/didemo/service/PrimarySpanishGreetingService.java
@@ -6,10 +6,10 @@ import org.springframework.stereotype.Service;
 
 @Service
 @Primary
-@Profile("en")
-public class PrimaryGreetingService implements GreetingService {
+@Profile("es")
+public class PrimarySpanishGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
-        return "Hello - Primary greeting service";
+        return "Hello - Servicio de Saludo Primario";
     }
 }
