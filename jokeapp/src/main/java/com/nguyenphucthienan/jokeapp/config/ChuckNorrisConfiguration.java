3b4bb115d33936e2899dfb27823b732258dd0ad8
Nguyen Phuc Thien An <npta97@gmail.com>
2018-07-08 11:18:12 +0700

    Spring configuration using Java class

---
+++ jokeapp/src/main/java/com/nguyenphucthienan/jokeapp/config/ChuckNorrisConfiguration.java
@@ -0,0 +1,13 @@
+package com.nguyenphucthienan.jokeapp.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChuckNorrisConfiguration {
+    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes() {
+        return new ChuckNorrisQuotes();
+    }
+}
--- jokeapp/src/main/java/com/nguyenphucthienan/jokeapp/service/JokeServiceImpl.java
+++ jokeapp/src/main/java/com/nguyenphucthienan/jokeapp/service/JokeServiceImpl.java
@@ -1,14 +1,16 @@
 package com.nguyenphucthienan.jokeapp.service;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JokeServiceImpl implements JokeService {
     private final ChuckNorrisQuotes chuckNorrisQuotes;
 
-    public JokeServiceImpl() {
-        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    @Autowired
+    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
     }
 
     @Override
