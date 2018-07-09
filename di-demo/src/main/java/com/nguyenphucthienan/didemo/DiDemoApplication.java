db6589a0439e1ac59d3c284a14da48c27ccb3b07
Nguyen Phuc Thien An <npta97@gmail.com>
2018-07-09 19:18:50 +0700

    Property Source

--- di-demo/src/main/java/com/nguyenphucthienan/didemo/DiDemoApplication.java
+++ di-demo/src/main/java/com/nguyenphucthienan/didemo/DiDemoApplication.java
@@ -1,9 +1,10 @@
 package com.nguyenphucthienan.didemo;
 
-import com.nguyenphucthienan.didemo.controller.ConstructorInjectedController;
-import com.nguyenphucthienan.didemo.controller.MyController;
-import com.nguyenphucthienan.didemo.controller.PropertyInjectedController;
-import com.nguyenphucthienan.didemo.controller.SetterInjectedController;
+// import com.nguyenphucthienan.didemo.controller.ConstructorInjectedController;
+// import com.nguyenphucthienan.didemo.controller.MyController;
+// import com.nguyenphucthienan.didemo.controller.PropertyInjectedController;
+// import com.nguyenphucthienan.didemo.controller.SetterInjectedController;
+import com.nguyenphucthienan.didemo.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -14,11 +15,14 @@ public class DiDemoApplication {
     public static void main(String[] args) {
         ApplicationContext context =  SpringApplication.run(DiDemoApplication.class, args);
 
-        MyController controller = (MyController) context.getBean("myController");
-        System.out.println(controller.hello());
+        // MyController controller = (MyController) context.getBean("myController");
+        // System.out.println(controller.hello());
 
-        System.out.println(context.getBean(PropertyInjectedController.class).sayHello());
-        System.out.println(context.getBean(SetterInjectedController.class).sayHello());
-        System.out.println(context.getBean(ConstructorInjectedController.class).sayHello());
+        // System.out.println(context.getBean(PropertyInjectedController.class).sayHello());
+        // System.out.println(context.getBean(SetterInjectedController.class).sayHello());
+        // System.out.println(context.getBean(ConstructorInjectedController.class).sayHello());
+
+        FakeDataSource fakeDataSource = context.getBean(FakeDataSource.class);
+        System.out.println(fakeDataSource.getUsername());
     }
 }
---
+++ di-demo/src/main/java/com/nguyenphucthienan/didemo/config/PropertyConfig.java
@@ -0,0 +1,35 @@
+package com.nguyenphucthienan.didemo.config;
+
+import com.nguyenphucthienan.didemo.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+@PropertySource("classpath:datasource.properties")
+public class PropertyConfig {
+    @Value("${user}")
+    String user;
+
+    @Value("${password}")
+    String password;
+
+    @Value("${dburl}")
+    String url;
+
+    @Bean
+    public FakeDataSource fakeDataSource() {
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setUsername(user);
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setUrl(url);
+        return fakeDataSource;
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+}
