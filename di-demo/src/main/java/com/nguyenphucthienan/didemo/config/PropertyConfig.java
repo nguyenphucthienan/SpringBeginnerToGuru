d3d548e51b2a38159996897c22617f92551326d5
Nguyen Phuc Thien An <npta97@gmail.com>
2018-07-10 20:15:19 +0700

    Spring Environment Properties

--- di-demo/src/main/java/com/nguyenphucthienan/didemo/config/PropertyConfig.java
+++ di-demo/src/main/java/com/nguyenphucthienan/didemo/config/PropertyConfig.java
@@ -1,28 +1,34 @@
 package com.nguyenphucthienan.didemo.config;
 
 import com.nguyenphucthienan.didemo.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
-    @Value("${user}")
+    @Autowired
+    Environment env;
+
+    @Value("${npta.username}")
     String user;
 
-    @Value("${password}")
+    @Value("${npta.password}")
     String password;
 
-    @Value("${dburl}")
+    @Value("${npta.dburl}")
     String url;
 
     @Bean
     public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUsername(user);
+        // fakeDataSource.setUsername(user);
+        fakeDataSource.setUsername(env.getProperty("TEST_USERNAME"));
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
