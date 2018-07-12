544a0f76dca101b71ea8a5e13a5a7848c38d3f6e
Nguyen Phuc Thien An <npta97@gmail.com>
2018-07-12 09:48:59 +0700

    Add Spring Data Repositories

---
+++ recipe-app/src/main/java/com/nguyenphucthienan/recipeapp/repository/RecipeRepository.java
@@ -0,0 +1,7 @@
+package com.nguyenphucthienan.recipeapp.repository;
+
+import com.nguyenphucthienan.recipeapp.domain.Recipe;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RecipeRepository extends CrudRepository<Recipe, Long> {
+}
---
+++ recipe-app/src/main/java/com/nguyenphucthienan/recipeapp/repository/UnitOfMeasureRepository.java
@@ -0,0 +1,7 @@
+package com.nguyenphucthienan.recipeapp.repository;
+
+import com.nguyenphucthienan.recipeapp.domain.UnitOfMeasure;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+}
