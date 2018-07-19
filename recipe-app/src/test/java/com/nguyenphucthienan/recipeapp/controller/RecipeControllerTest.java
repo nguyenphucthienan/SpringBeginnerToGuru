7fc80ac98455b9153cbbcdf30fe386cfb6fd4479
Nguyen Phuc Thien An <npta97@gmail.com>
2018-07-19 10:08:38 +0700

    Add unit test

--- recipe-app/src/test/java/com/nguyenphucthienan/recipeapp/controller/RecipeControllerTest.java
+++ recipe-app/src/test/java/com/nguyenphucthienan/recipeapp/controller/RecipeControllerTest.java
@@ -70,4 +70,17 @@ public class RecipeControllerTest {
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/recipe/2/show"));
     }
+
+    @Test
+    public void testGetUpdateRecipeForm() throws Exception {
+        RecipeCommand recipeCommand = new RecipeCommand();
+        recipeCommand.setId(2L);
+
+        when(recipeService.findCommandById(anyLong())).thenReturn(recipeCommand);
+
+        mockMvc.perform(get("/recipe/2/update"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/recipe-form"))
+                .andExpect(model().attributeExists("recipe"));
+    }
 }
