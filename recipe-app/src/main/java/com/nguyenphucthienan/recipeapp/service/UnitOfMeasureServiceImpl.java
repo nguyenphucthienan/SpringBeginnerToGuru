e013da5f1d5092b9a1ae4365b7df86ae8b1296dd
Nguyen Phuc Thien An <npta97@gmail.com>
2018-07-19 22:25:50 +0700

    Add unit test

--- recipe-app/src/main/java/com/nguyenphucthienan/recipeapp/service/UnitOfMeasureServiceImpl.java
+++ recipe-app/src/main/java/com/nguyenphucthienan/recipeapp/service/UnitOfMeasureServiceImpl.java
@@ -10,12 +10,12 @@ import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 @Service
-public class UnitOfMeasureImpl implements UnitOfMeasureService {
+public class UnitOfMeasureServiceImpl implements UnitOfMeasureService {
     private final UnitOfMeasureRepository unitOfMeasureRepository;
     private final UnitOfMeasureToUnitOfMeasureCommand unitOfMeasureToUnitOfMeasureCommand;
 
-    public UnitOfMeasureImpl(UnitOfMeasureRepository unitOfMeasureRepository,
-                             UnitOfMeasureToUnitOfMeasureCommand unitOfMeasureToUnitOfMeasureCommand) {
+    public UnitOfMeasureServiceImpl(UnitOfMeasureRepository unitOfMeasureRepository,
+                                    UnitOfMeasureToUnitOfMeasureCommand unitOfMeasureToUnitOfMeasureCommand) {
         this.unitOfMeasureRepository = unitOfMeasureRepository;
         this.unitOfMeasureToUnitOfMeasureCommand = unitOfMeasureToUnitOfMeasureCommand;
     }
---
+++ recipe-app/src/test/java/com/nguyenphucthienan/recipeapp/service/UnitOfMeasureServiceImplTest.java
@@ -0,0 +1,57 @@
+package com.nguyenphucthienan.recipeapp.service;
+
+import com.nguyenphucthienan.recipeapp.command.UnitOfMeasureCommand;
+import com.nguyenphucthienan.recipeapp.converter.UnitOfMeasureToUnitOfMeasureCommand;
+import com.nguyenphucthienan.recipeapp.domain.UnitOfMeasure;
+import com.nguyenphucthienan.recipeapp.repository.UnitOfMeasureRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class UnitOfMeasureServiceImplTest {
+    UnitOfMeasureToUnitOfMeasureCommand unitOfMeasureToUnitOfMeasureCommand;
+    UnitOfMeasureService unitOfMeasureService;
+
+    @Mock
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public UnitOfMeasureServiceImplTest() {
+        unitOfMeasureToUnitOfMeasureCommand = new UnitOfMeasureToUnitOfMeasureCommand();
+    }
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        unitOfMeasureService = new UnitOfMeasureServiceImpl(unitOfMeasureRepository, unitOfMeasureToUnitOfMeasureCommand);
+    }
+
+    @Test
+    public void testGetAllUnitOfMeasures() {
+        Set<UnitOfMeasure> unitOfMeasures = new HashSet<>();
+
+        UnitOfMeasure unitOfMeasure1 = new UnitOfMeasure();
+        unitOfMeasure1.setId(1L);
+        unitOfMeasures.add(unitOfMeasure1);
+
+        UnitOfMeasure unitOfMeasure2 = new UnitOfMeasure();
+        unitOfMeasure2.setId(2L);
+        unitOfMeasures.add(unitOfMeasure2);
+
+        when(unitOfMeasureRepository.findAll()).thenReturn(unitOfMeasures);
+
+        Set<UnitOfMeasureCommand> commands = unitOfMeasureService.getAllUnitOfMeasures();
+
+        assertEquals(2, commands.size());
+        verify(unitOfMeasureRepository, times(1)).findAll();
+    }
+}
