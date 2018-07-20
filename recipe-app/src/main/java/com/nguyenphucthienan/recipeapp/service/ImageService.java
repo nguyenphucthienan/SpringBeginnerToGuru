b0a374526712bf0c8a89b0a69f2d6f1e7e3cd2ce
Nguyen Phuc Thien An <npta97@gmail.com>
2018-07-20 09:54:12 +0700

    Upload image files with Spring MVC

---
+++ recipe-app/src/main/java/com/nguyenphucthienan/recipeapp/service/ImageService.java
@@ -0,0 +1,7 @@
+package com.nguyenphucthienan.recipeapp.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface ImageService {
+    void saveImageFile(Long recipeId, MultipartFile file);
+}
