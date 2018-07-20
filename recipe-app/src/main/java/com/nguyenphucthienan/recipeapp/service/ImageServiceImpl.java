5d41c97316d20cf4e1899f8830214a847eaa8319
Nguyen Phuc Thien An <npta97@gmail.com>
2018-07-20 10:30:01 +0700

    Persist images to database

--- recipe-app/src/main/java/com/nguyenphucthienan/recipeapp/service/ImageServiceImpl.java
+++ recipe-app/src/main/java/com/nguyenphucthienan/recipeapp/service/ImageServiceImpl.java
@@ -1,5 +1,7 @@
 package com.nguyenphucthienan.recipeapp.service;
 
+import com.nguyenphucthienan.recipeapp.domain.Recipe;
+import com.nguyenphucthienan.recipeapp.repository.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -7,8 +9,29 @@ import org.springframework.web.multipart.MultipartFile;
 @Slf4j
 @Service
 public class ImageServiceImpl implements ImageService {
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
     @Override
     public void saveImageFile(Long recipeId, MultipartFile file) {
-        log.debug("Received a file");
+        try {
+            Recipe recipe = recipeRepository.findById(recipeId).get();
+
+            Byte[] byteOfObjects = new Byte[file.getBytes().length];
+
+            int i = 0;
+            for (byte b : file.getBytes()) {
+                byteOfObjects[i++] = b;
+            }
+
+            recipe.setImage(byteOfObjects);
+            recipeRepository.save(recipe);
+        } catch (Exception e) {
+            // TODO Implement error handling
+            log.error("Error occurred: ", e);
+        }
     }
 }
