b865e9d25f2d0a73b42c094ff299bf28a277412f
Nguyen Phuc Thien An <npta97@gmail.com>
2020-05-27 13:38:11 +0700

    Init spring-webflux-rest project

---
+++ spring-webflux-rest/src/main/java/com/nguyenphucthienan/springwebfluxrest/SpringWebFluxRestApplication.java
@@ -0,0 +1,12 @@
+package com.nguyenphucthienan.springwebfluxrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebFluxRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebFluxRestApplication.class, args);
+    }
+}
---
+++ spring-webflux-rest/src/test/java/com/nguyenphucthienan/springwebfluxrest/SpringWebFluxRestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nguyenphucthienan.springwebfluxrest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringWebFluxRestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
